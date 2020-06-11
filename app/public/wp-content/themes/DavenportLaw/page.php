dec421742daaf27f7791c8a78cbd8d7b8e932159
Edgar Limon <edgarlimon805@gmail.com>
2020-06-10 18:18:19 -0700

    new

--- app/public/wp-content/themes/DavenportLaw/page.php
+++ app/public/wp-content/themes/DavenportLaw/page.php
@@ -25,24 +25,18 @@
     </div>
     <div class="col-md-10">
       <h1 class='pt-3' style='border-bottom:2px solid black'><?php the_title(); ?></h1>
-      <img src="<?php get_theme_file_uri('/images/');?>" alt="">
     </div>
   </div>
 </div>
 
 
 <div class="container-fluid">
+  .
   <div class="row">
     <div class="col-md-2 order-2 order-md-1">
-      <!-- <h3>Services</h3>
-      <ul>
-        <li>Harassments</li>
-        <li>Wage</li>
-        <li>Law</li>
-      </ul> -->
       <?php  get_template_part('template-parts/side-nav'); ?>
     </div>
-    <div class="col-md-10 order-1  order-md-2">
+    <div class="col-md-8 order-1  order-md-2">
     
       <?php the_content(); ?>
     </div>
